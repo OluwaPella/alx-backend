0dd1c4b6a126cd2399b47b1aa838e0502af7e805
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-05-23 17:37:33 +0100

    add:using FIFO algorithm  method for cache

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" LIFO Caching
+"""
+from base_caching import BaseCaching
+
+class LIFOCache(BaseCaching):
+    """ LIFO Caching
+    """
+    def __init__(self):
+        """ Initiliaze
+        """
+        super().__init__()
+        self.cache_data = {}
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            last_key, _ = self.cache_data.popitem()
+            print("DISCARD: {}".format(last_key))
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        return self.cache_data.get(key, None)
