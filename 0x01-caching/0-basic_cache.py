db3d8f593f5a7cfb1edde6aa92e9b6d6d5b0d553
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-05-23 03:32:04 +0100

    add:get and put method for cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -5,9 +5,8 @@ class BasicCache(BaseCaching):
 
     def put(self, key, item):
         """ Add an item in the cache"""
-        self.cache_data[key] = item
-        if key is None or item is None:
-            return None
+        if key and item:
+            self.cache_data[key] = item
         
     def get(self, key):
         """ Get an item by key and return it's value
