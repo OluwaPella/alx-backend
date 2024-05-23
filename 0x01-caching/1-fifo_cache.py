5bf515edd3604535561068019546f2d2562617a7
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-05-23 14:34:20 +0100

    add:get and put method for cache

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from base_caching import BaseCaching
+
+class FIFOCache( BaseCaching):
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) >  BaseCaching.MAX_ITEMS:
+            first_key = list(self.cache_data.keys())[0]
+            
+
+    def get(self, key):
+        return self.cache_data.get(key, None)
+
+
+
+
+    
