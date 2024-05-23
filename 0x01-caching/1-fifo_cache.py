5cc56d3c1aa2f745187b38da43fcae3785d2dcf7
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-05-23 16:04:31 +0100

    add:using FIFO algorithm  method for cache

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 from base_caching import BaseCaching
-from collections import OrderedDict()
+from collections import OrderedDict 
 
 class FIFOCache( BaseCaching):
+    """ FIFOCache defines"""
     def __init__(self):
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
+        """ Add an item in the cache"""
         if key  is None or item is None:
              self.cache_data[key] = item
         if len(self.cache_data) >  BaseCaching.MAX_ITEMS:
                 first_key, _ = self.cache_data.popitem(False)
                 print("DISCARD: {}".format(first_key))
     def get(self, key):
+        """ Get an item by key"""
         return self.cache_data.get(key, None)
 
 
