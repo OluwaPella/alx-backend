b80b593928f895baf83deb8b8c61fec107755b79
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-05-26 14:59:35 +0100

    add:2-lifo_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -4,11 +4,9 @@
 from base_caching import BaseCaching
 
 class BasicCache(BaseCaching):
-
-
     """ BasicCache defines"""
     def put(self, key, item):
-        """ Add an item in the cache"""
+        """Add an item in the cache"""
         if key is None or item is None:
             return
         self.cache_data[key] = item
