64adf2c54777a62a2d679d6b83a9e19e1d33aee6
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-05-29 03:19:38 +0100

    4-mru_cache.py

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
 """MRUCache
 """
-
 from collections import OrderedDict
 from base_caching import BaseCaching
 
 class MRUCache(BaseCaching):
     """
-    MRUCache class
+    MRUCache is a caching algorithm
     """
 
     def __init__(self):
         """Initiliaze
         """
-
         super().__init__()
         self.cache_data = OrderedDict()
-
-
+    
     def put(self, key, item):
         """adding item to cache
         """
@@ -25,7 +22,7 @@ class MRUCache(BaseCaching):
             return
         if key not in self.cache_data:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                mru_key, _ = self.cache_data.popitem(key, last=False)
+                mru_key, _ = self.cache_data.popitem(last=False)
                 print("DISCARD:", mru_key)
                 self.cache_data[key] = item
                 self.cache_data.move_to_end(key, last=False)
