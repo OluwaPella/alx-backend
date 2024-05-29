d80885efd9be1d462b800128ebd74aac920ae6e8
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-05-29 18:56:03 +0100

    add: 1-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
-"""initialize flask app 
-to use babel
+"""
+Task1 -initialize flask app 
+to use babel.
 """
 from flask import Flask, render_template
-from babel import Babel
+from flask_babel import Babel
 
 
 app = Flask(__name__)
 
 class Config:
-    """Represents a Flask Babel configuration.
+    """
+    Represents a Flask Babel configuration.
     """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
@@ -21,9 +23,11 @@ app.config.from_object(Config)
 
 babel = Babel(app)
 
+
 @app.route('/')
 def get_index():
-    """return 1-index.html
+    """
+    return 1-index.html.
     """
     return render_template('1-index.html')
 
