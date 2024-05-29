031662f05aca1440dba8d11648df9d32686e090c
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-05-29 16:21:33 +0100

    add: appy.py file

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,7 +7,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def get_index():
     """return index.html
     """
     return render_template('0-index.html')
