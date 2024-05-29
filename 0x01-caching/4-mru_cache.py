e0f2ab5404ac6666b3d7a0be9a66b35042bcc3ee
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-05-29 02:59:56 +0100

    4-mru_cache.py

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -11,15 +11,17 @@ class MRUCache(BaseCaching):
     """
 
     def __init__(self):
+        """Initiliaze"""
         super().__init__()
         self.cache_data = OrderedDict()
 
 
     def put(self, key, item):
-        """ adding item to cache"""
+        """adding item to cache
+        """
         if key is None or item is None:
             return
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
             mru_key = self.cache_data.popitem(key, last=False)
             print("DISCARD", mru_key)
             self.cache_data.move_to_end(key, last=False)
@@ -28,7 +30,8 @@ class MRUCache(BaseCaching):
 
         
     def get(self, key):
-        """getting data from cache using the key"""
+        """getting data from cache using the key
+        """
         if key is not None and key in self.cache_data:
             self.cache_data.move_to_end(key, last=False)
         return self.cache_data(key, None)
