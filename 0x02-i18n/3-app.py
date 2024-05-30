46d0aa8612f9ebb837682c990bf3a0578a52b522
oolaosebikan360@gmail.com <oolaosebikan350@gmail.com>
2024-05-30 15:37:01 +0100

    3-app.py

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+"""Task1 -initialize flask app 
+to use babel.
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+class Config:
+    """Represents a Flask Babel configuration.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    use he locale from the header
+    or we support de/fr/en.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route('/')
+def get_index():
+    """
+    return 2-index.html.
+    """
+    return render_template('3-index.html')
+
+if __name__ == "__main__":
+    
+    app.run(port=5000, host='0.0.0.0')
